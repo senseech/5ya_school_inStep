065b730dbdc4927b7960642db67e52731e5749a1
senseech <maxic1234@gmail.com>
2022-05-30 15:45:47 +0300

    2com

--- src/main/java/OneStepAwayFromIdeal.java
+++ src/main/java/OneStepAwayFromIdeal.java
@@ -14,31 +14,52 @@ public class OneStepAwayFromIdeal {
         byte[] inputBuffer = new byte[fileInputStream.available()];
         fileInputStream.read(inputBuffer, 0, fileInputStream.available());
         ArrayList<Character> arrayChars = new ArrayList();
+        ArrayList<Character>arrayChars_tmp = new ArrayList();
         for (byte b : inputBuffer) arrayChars.add((char) b);
 
 
 
+        if (CheckString(arrayChars)){
+            System.out.println(-1);
+        } else {
+            for(int i = 0; i < arrayChars.size(); i++){
+                arrayChars_tmp = arrayChars;
+                if (arrayChars_tmp.get(i).equals("(") |
+                        (arrayChars_tmp.get(i).equals(")"))){
+                    arrayChars_tmp.remove(i);
+                    if (CheckString(arrayChars_tmp)){
+                        System.out.println(i+1);
+                        break;
+                    }
+                }
+                if (i == arrayChars.size()) System.out.println(-1);
+            }
+        }
 
 
-
-
-
-        System.out.println(CheckString(arrayChars));
-        arrayChars.forEach(System.out::print);
-        System.out.println();
+//        System.out.println(CheckString(arrayChars));
+//        arrayChars.forEach(System.out::print);
+//
+//        arrayChars.remove(5);
+//        System.out.println();
+//        arrayChars.forEach(System.out::print);
+//        System.out.println();
+//        System.out.println(arrayChars.remove(5));
+//        arrayChars.forEach(System.out::print);
     }
     //хреновый поиск
     //не ищет )()(
     //Вроде ищет
-    public static int CheckString(ArrayList<Character> arrayList){
+    public static boolean CheckString(ArrayList<Character> arrayList){
         int[] countBrackets = {0,0,0};
-        boolean checkSide = false;
+        final boolean[] checkSide = {false};
         boolean[] checkBrackets = {false, false, false};
         if (arrayList.contains("=")){
             arrayList.forEach(ch ->{
-                if (!checkSide){
+                if (!checkSide[0]){
                     if (ch.equals("(")) countBrackets[0]++;
                     if (ch.equals(")")) countBrackets[0]--;
+                    if (ch.equals("=")) checkSide[0] = true;
                     if (countBrackets[0] < 0) checkBrackets[0] = true;
                 }
                 else {
@@ -57,10 +78,10 @@ public class OneStepAwayFromIdeal {
         }
         if ((Arrays.equals(countBrackets, new int[]{0, 0, 0})) &
                 (Arrays.equals(checkBrackets, new boolean[]{false, false, false}))){
-            return 1;
+            return true;
         }
         else
-            return 0;
+            return false;
 
     }
 }
